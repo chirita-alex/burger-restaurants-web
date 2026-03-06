1f7c5c40af19103a6e2dade456099f0645eebab0
Chirita <alexandru.chirita@ownzones.com>
2026-03-06 10:00:01 +0200

    fix: update Notice component for multiple uses with images

--- src/shared/Notice.tsx
+++ src/shared/Notice.tsx
@@ -1,28 +1,43 @@
 import { Link, isRouteErrorResponse, useRouteError } from "react-router-dom";
+import errorSvg from "../assets/icons/error.svg";
+import notFoundSvg from "../assets/icons/not-found.svg";
+import unauthorizedSvg from "../assets/icons/unauthorized.svg";
+import emptyFilesSvg from "../assets/icons/empty-files.svg";
+import "./Notice.scss";
 
-const STATUS_MESSAGES: Record<number, { heading: string; message: string }> = {
-  400: { heading: "Bad request", message: "The request was invalid. Please check the URL and try again." },
-  401: { heading: "Unauthorized", message: "You need to be logged in to view this page." },
-  403: { heading: "Forbidden", message: "You don't have permission to view this page." },
-  404: { heading: "Page not found", message: "The page you're looking for doesn't exist." },
-  500: { heading: "Server error", message: "Something went wrong on our end. Please try again later." },
+type NoticeConfig = {
+  heading: string;
+  message: string;
+  icon: string;
 };
 
-const DEFAULT_STATUS = { heading: "Something went wrong", message: "An unexpected error occurred." };
+const STATUS_CONFIG: Record<number, NoticeConfig> = {
+  400: { heading: "Bad request", message: "The request was invalid. Please check the URL and try again.", icon: emptyFilesSvg },
+  401: { heading: "Unauthorized", message: "You need to be logged in to view this page.", icon: unauthorizedSvg },
+  403: { heading: "Forbidden", message: "You don't have permission to view this page.", icon: unauthorizedSvg },
+  404: { heading: "Page not found", message: "The page you're looking for doesn't exist.", icon: notFoundSvg },
+  500: { heading: "Server error", message: "Something went wrong on our end. Please try again later.", icon: errorSvg },
+};
 
-// TODO: Better UI for error states, maybe with different images for different status codes
+const DEFAULT_CONFIG: NoticeConfig = {
+  heading: "Something went wrong",
+  message: "An unexpected error occurred.",
+  icon: errorSvg,
+};
+
 const Notice = () => {
   const error = useRouteError();
 
-  const { heading, message } = isRouteErrorResponse(error)
-    ? (STATUS_MESSAGES[error.status] ?? DEFAULT_STATUS)
-    : DEFAULT_STATUS;
+  const { heading, message, icon } = isRouteErrorResponse(error)
+    ? (STATUS_CONFIG[error.status] ?? DEFAULT_CONFIG)
+    : DEFAULT_CONFIG;
 
   return (
-    <section aria-labelledby="error-heading">
-      <h1 id="error-heading">{heading}</h1>
-      <p>{message}</p>
-      <Link to="/">Go to home page</Link>
+    <section className="notice" aria-labelledby="notice-heading">
+      <img className="notice__icon" src={icon} alt="" aria-hidden="true" />
+      <h1 className="notice__heading" id="notice-heading">{heading}</h1>
+      <p className="notice__message">{message}</p>
+      <Link className="notice__link" to="/">Go to home page</Link>
     </section>
   );
 };
