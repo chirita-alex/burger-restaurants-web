53c6a6aa494120d74280cd5b020dfd18cf12361a
Chirita <alexandru.chirita@ownzones.com>
2026-03-08 08:04:28 +0200

    chore: add autoprefixer on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
